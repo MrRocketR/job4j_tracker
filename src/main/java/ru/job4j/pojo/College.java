615996e4569c3a2b6c131d1dfc2319aa4dfa1c6a
MrRocketR <kuptovns@gmail.com>
2021-08-26 22:43:15 +0300

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,11 @@
+package ru.job4j.pojo;
+
+public class College {
+    public static void main(String[] args) {
+        Student ivan = new Student();
+        ivan.setFirstName("Ivan");
+        ivan.setLastName("Ivanov");
+        ivan.setGroup(182);
+        System.out.println(ivan.getFirstName() + " " + ivan.getLastName() + " from " + ivan.getGroup());
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Student.java
@@ -0,0 +1,51 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class Student {
+    private String firstName;
+    private String secondName;
+    private String lastName;
+    private int group;
+    private Date dateOfBirth;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getGroup() {
+        return group;
+    }
+
+    public void setGroup(int group) {
+        this.group = group;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
