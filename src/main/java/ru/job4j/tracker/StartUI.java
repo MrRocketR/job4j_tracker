dfe08b51e066d402a6971f62daf129d3fc9044e9
MrRocketR <kuptovns@gmail.com>
2021-09-15 22:26:29 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,6 +6,8 @@ public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
         System.out.println(item.getCurrentDateTime().format(DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss")));
+        Item item2 = new Item();
+        System.out.println(item2);
     }
 
 }
