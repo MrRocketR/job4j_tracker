08da21999533dddf3b10d277832384fa1af470c9
MrRocketR <kuptovns@gmail.com>
2021-09-21 23:43:22 +0300

    9. Написать тесты на StartUI. [#785]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 
 public class StartUITest {
 
-/*    @Test
+    @Test
     public void whenCreateItem() {
         Input in = new StubInput(
                 new String[] {"0", "Item name", "1"}
@@ -21,7 +21,42 @@ public class StartUITest {
         new StartUI().init(in, tracker, actions);
         assertThat(tracker.findAll()[0].getName(), is("Item name"));
     }
-}*/
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        /* Добавим в tracker новую заявку */
+        Item item = tracker.add(new Item("Replaced item"));
+        /* Входные данные должны содержать ID добавленной заявки item.getId() */
+        String replacedName = "New item name";
+        Input in = new StubInput(
+                new String[] {"0" , String.valueOf(item.getId()),replacedName,"1", }
+        );
+        UserAction[] actions = {
+                new ReplaceAction(),
+                new ExitAction()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findById(item.getId()).getName(), is(replacedName));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+         //Добавим в tracker новую заявку
+        Item item = tracker.add(new Item("Deleted item"));
+         //Входные данные должны содержать ID добавленной заявки item.getId()
+        Input in = new StubInput(
+                new String[] {"0",  String.valueOf(item.getId()) , "1"}
+        );
+        UserAction[] actions = {
+                new DeleteAction(),
+                new ExitAction()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findById(item.getId()), is(nullValue()));
+    }
+}
 /*    @Test
     public void whenReplaceItem() {
         Tracker tracker = new Tracker();
@@ -47,8 +82,6 @@ public class StartUITest {
         assertNull(deleted);
        // assertThat(deleted, is(IsNull.nullValue())); <- другой вариант
         //assertThat(deleted.getId(), is(nullValue())); <- нельзя вызывать getId*/
-
-    }
 
 
 
