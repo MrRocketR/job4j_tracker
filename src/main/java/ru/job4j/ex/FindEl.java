7fdf22aa60ced0123b94310bf2bb64aec140b480
MrRocketR <kuptovns@gmail.com>
2021-09-26 21:05:07 +0300

    0.4. Пользовательские исключения. [#219367 #207594]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -6,7 +6,11 @@ public class FindEl {
         for (int i = 0; i < value.length; i++) {
                 if (value[i].equals(key)) {
                     rsl = i;
+                    break;
                 }
+        }
+        if (rsl == -1) {
+           throw new ElementNotFoundException("element now found!");
         }
         return rsl;
     }
