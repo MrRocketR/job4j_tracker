ed08487a548e5d8cac5925bf641ce0ef22bc68c6
MrRocketR <kuptovns@gmail.com>
2021-10-14 22:54:35 +0300

    1. Паспорт и жители [#212656 #215742]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -2,8 +2,14 @@ package ru.job4j.collection;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class PassportOfficeTest {
 
@@ -13,5 +19,15 @@ public class PassportOfficeTest {
         PassportOffice office = new PassportOffice();
         office.add(citizen);
         assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void whenIdentical() {
+        Citizen citizen1 = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("2f44a", "Simple Petr");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen1);
+        assertFalse(office.add(citizen2));
+
     }
 }
