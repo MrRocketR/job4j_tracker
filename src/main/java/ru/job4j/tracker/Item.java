8e5e87a1fbea6a4b6f523df21ae9fcd630a95009
MrRocketR <kuptovns@gmail.com>
2021-10-21 01:40:21 +0300

    Для CodeCove

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -50,7 +50,7 @@ public class Item implements Comparable<Item> {
                 + id
                 + ", name='" + name
                 + '\''
-                +  ", created=" + created.format(FORMATTER)
+                + ", created=" + created.format(FORMATTER)
                 + '}';
     }
 
@@ -61,8 +61,12 @@ public class Item implements Comparable<Item> {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Item item = (Item) o;
         return id == item.id && Objects.equals(name, item.name);
     }
