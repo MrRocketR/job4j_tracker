3c665d21a271938dac70319734c345f39a4c7581
MrRocketR <kuptovns@gmail.com>
2021-11-02 23:07:56 +0300

    1. Фильтрация учеников [#110058]

---
+++ src/main/java/ru/job4j/streams/School.java
@@ -0,0 +1,12 @@
+package ru.job4j.streams;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class School {
+
+    public List<Student> collect(List<Student> students, Predicate<Student> predict) {
+        return new ArrayList<Student>();
+    }
+}
