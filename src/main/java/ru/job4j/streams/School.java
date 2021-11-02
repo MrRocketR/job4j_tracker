13e8b25f3f9dc5b0a6bf5c07ac5f615b507e63de
MrRocketR <kuptovns@gmail.com>
2021-11-02 23:26:07 +0300

    1. Фильтрация учеников [#110058 #223865]

--- src/main/java/ru/job4j/streams/School.java
+++ src/main/java/ru/job4j/streams/School.java
@@ -3,10 +3,13 @@ package ru.job4j.streams;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class School {
 
     public List<Student> collect(List<Student> students, Predicate<Student> predict) {
-        return new ArrayList<Student>();
+        List<Student> out = new ArrayList<>();
+        out = students.stream().filter(predict).collect(Collectors.toList());
+        return out;
     }
 }
