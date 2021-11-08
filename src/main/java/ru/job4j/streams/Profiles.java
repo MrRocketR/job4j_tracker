b7d4870909d4d3168b01d00b9ef6ec3f91771474
MrRocketR <kuptovns@gmail.com>
2021-11-08 21:49:03 +0300

    2. Список адресов [#110062 #225980]

--- src/main/java/ru/job4j/streams/Profiles.java
+++ src/main/java/ru/job4j/streams/Profiles.java
@@ -6,8 +6,7 @@ import java.util.stream.Collectors;
 
 public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
-        List<Address> addresses = new ArrayList<>();
-        return profiles.stream().map(p -> p.getAddress()).collect(Collectors.toList());
+        return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
     }
 
 }
