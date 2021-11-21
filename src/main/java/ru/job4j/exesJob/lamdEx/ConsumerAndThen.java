2f291e409da9cc42c83100df84debb86f70b632e
MrRocketR <kuptovns@gmail.com>
2021-11-21 22:36:29 +0300

    3.1. Consumer. Вывод строки. Метод andThen()

---
+++ src/main/java/ru/job4j/exesJob/lamdEx/ConsumerAndThen.java
@@ -0,0 +1,16 @@
+package ru.job4j.exesJob.lamdEx;
+
+import java.util.function.Consumer;
+
+public class ConsumerAndThen {
+    public static Consumer<String> consumer(String input) {
+        Consumer<String> print = x -> System.out.print(x);
+        Consumer<String> ln = (a) -> System.out.println();
+        return print.andThen(ln);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("job4j");
+
+    }
+}
