6c65a134685f0e9355540686b774fcfca74e356a
MrRocketR <kuptovns@gmail.com>
2021-11-21 21:56:22 +0300

    Predicate упражнения

---
+++ src/main/java/ru/job4j/exesJob/lamdEx/PredicateCheckEvenAndPositive.java
@@ -0,0 +1,14 @@
+package ru.job4j.exesJob.lamdEx;
+
+import java.util.function.Predicate;
+
+public class PredicateCheckEvenAndPositive {
+
+    public static boolean check(int num) {
+        return check(n -> (n % 2 == 0 && n > 0), num);
+    }
+
+    private static boolean check(Predicate<Integer> predicate, int num) {
+        return predicate.test(num);
+    }
+}
---
+++ src/main/java/ru/job4j/exesJob/lamdEx/PredicateCheckPositive.java
@@ -0,0 +1,18 @@
+package ru.job4j.exesJob.lamdEx;
+
+import java.util.function.Predicate;
+
+public class PredicateCheckPositive {
+
+    public static boolean check(int num) {
+        return check(n -> n > 0, num);
+    }
+
+    private static boolean check(Predicate<Integer> predicate, int num) {
+        return predicate.test(num);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(check(2));
+    }
+}
