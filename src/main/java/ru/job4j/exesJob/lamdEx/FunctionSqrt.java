f3cc18351e439ea5c1af34637f012af3c46fdc31
MrRocketR <kuptovns@gmail.com>
2021-11-21 22:15:12 +0300

    Упражнения на Fuction

---
+++ src/main/java/ru/job4j/exesJob/lamdEx/FunctionSqrt.java
@@ -0,0 +1,15 @@
+package ru.job4j.exesJob.lamdEx;
+
+import java.util.function.Function;
+
+public class FunctionSqrt {
+
+    public static double calculate(double x) {
+        return calculate(a -> Math.sqrt(a), x);
+    }
+
+    public static double calculate(Function<Double, Double> y, double x) {
+        return y.apply(x);
+    }
+
+}
