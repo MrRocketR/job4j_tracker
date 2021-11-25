3c2ffd5b43712ac3f6957701a2889ad305166663
MrRocketR <kuptovns@gmail.com>
2021-11-25 22:10:05 +0300

    5.4. Optional. Метод ofNullable()

---
+++ src/main/java/ru/job4j/exesJob/lamd/lamdEx/OptionalOfNullable.java
@@ -0,0 +1,20 @@
+package ru.job4j.exesJob.lamd.lamdEx;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public class OptionalOfNullable {
+    public static Optional<String> findValue(List<String> strings, String value) {
+        Optional<String> out = Optional.empty();
+        Optional<String> v = Optional.ofNullable(value);
+        for (String s:strings) {
+            Optional<String> nS = Optional.ofNullable(s);
+            if (nS.equals(v)) {
+                out = nS;
+                break;
+            }
+        }
+        return out;
+    }
+}
