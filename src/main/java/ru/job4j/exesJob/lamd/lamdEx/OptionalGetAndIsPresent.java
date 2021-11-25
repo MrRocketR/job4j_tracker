b42afb410a27a3d4bd7436b2470da7b6f6fbac5a
MrRocketR <kuptovns@gmail.com>
2021-11-25 19:11:08 +0300

    Optional

---
+++ src/main/java/ru/job4j/exesJob/lamd/lamdEx/OptionalGetAndIsPresent.java
@@ -0,0 +1,30 @@
+package ru.job4j.exesJob.lamd.lamdEx;
+
+import java.util.Optional;
+
+public class OptionalGetAndIsPresent {
+
+    public static int get(int[] data, int el) {
+        Optional<Integer> out = indexOf(data, el);
+        if (out.isEmpty()) {
+            return -1;
+        } else {
+            return out.get();
+        }
+    }
+
+    private static Optional<Integer> indexOf(int[] data, int el) {
+        Optional<Integer> out = null;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == el) {
+                out = Optional.of(i);
+                out.get();
+                break;
+            }
+            out = Optional.empty();
+        }
+
+        return out;
+    }
+
+}
---
+++ src/main/java/ru/job4j/exesJob/lamd/lamdEx/OptionalOfAndEmpty.java
@@ -0,0 +1,16 @@
+package ru.job4j.exesJob.lamd.lamdEx;
+
+import java.util.List;
+import java.util.Optional;
+
+public class OptionalOfAndEmpty {
+    public static Optional<String> findValue(List<String> strings, String value) {
+        Optional<String> out;
+        if (strings.contains(value)) {
+            out = Optional.of(value);
+        } else {
+            out = Optional.empty();
+        }
+        return out;
+    }
+}
