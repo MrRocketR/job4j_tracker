8a8a2bcea31b30d0bf84820c991c4337694abbc0
MrRocketR <kuptovns@gmail.com>
2021-11-25 21:32:28 +0300

    5.3. Optional. Метод ifPresent()

---
+++ src/main/java/ru/job4j/exesJob/lamd/lamdEx/OptionalIfPresent.java
@@ -0,0 +1,31 @@
+package ru.job4j.exesJob.lamd.lamdEx;
+
+import java.util.Optional;
+
+public class OptionalIfPresent {
+
+    public static void ifPresent(int[] data) {
+        Optional<Integer> p = max(data);
+        p.ifPresent(val -> System.out.println("Max: " +  val));
+    }
+
+    private static Optional<Integer> max(int[] data) {
+        Optional<Integer> out;
+        if (data.length != 0) {
+            int max = 0;
+            for (int datum : data) {
+                if (datum > max) {
+                    max = datum;
+                }
+            }
+            out = Optional.of(max);
+
+        } else out = Optional.empty();
+        return out;
+    }
+
+    public static void main(String[] args) {
+        int [] data1 = {1, 2, 3};
+        ifPresent(data1);
+        }
+}
