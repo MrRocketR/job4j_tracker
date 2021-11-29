124060c94e7224da04db3cf8f942a3f9736ce2a6
MrRocketR <kuptovns@gmail.com>
2021-11-29 19:59:31 +0300

    4. Optional в Stream API. [#279130 #233760]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -2,39 +2,13 @@ package ru.job4j.bank;
 
 import java.util.*;
 
-/**
- * Класс описывает простые банковские операции
- * Состоит из 5 разных методов
- * @author Nikita Kuptsov
- * @version 1.0
- */
 public class BankService {
-    /**
-     * Хранение задания осуществляется в коллекции типа HashMap
-     */
-    private final Map<User, List<Account>> users = new HashMap<>();
 
-    /**
-     * Добавление нового пользователя
-     * @see User
-     * @param user имя нового пользователя
-     * Перед добавлением нового пользователя происходит проверка на его наличии в коллекции
-     */
+    private final Map<User, List<Account>> users = new HashMap<>();
 
     public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<>());
     }
-
-    /**
-     * Добавление нового счета:
-     * @param account имя нового счета
-     * по паспорту:
-     * @param passport номер паспорта в виде String
-     * Вызывается метод проверка наличия данного пользователя по паспорту
-     * В случае успешной проверки новый счет добавляется к user
-     * @see User
-     * @see Account
-     */
 
     public void addAccount(String passport, Account account) {
         Optional<User> user = findByPassport(passport);
@@ -48,12 +22,6 @@ public class BankService {
         }
     }
 
-    /**
-     * Поиск пользователя по паспорту:
-     * @param passport номер паспорта в виде String
-     * @return вовзращает пользователя User
-     * @see User
-     */
     public Optional<User> findByPassport(String passport) {
         return users.keySet()
                 .stream()
@@ -61,14 +29,6 @@ public class BankService {
                 .findFirst();
     }
 
-    /**
-     * Поиск пользователя по реквизитам и паспорту
-     * @param passport номер паспорта в виде String
-     * @param requisite номер реквизитов в виде String
-     * @return возвращает все счета данного пользователя
-     * @see User
-     * @see Account
-     */
     public Optional<Account> findByRequisite(String passport, String requisite) {
         Optional<User> user = findByPassport(passport);
         if (user.isPresent()) {
@@ -80,20 +40,6 @@ public class BankService {
         return Optional.empty();
     }
 
-    /**
-     * Метод переводит средства с одного счета на другой
-     * В качестве параметров небходим паспорт и реквизита исходного пользователя
-     * @param srcPassport номер паспорта отправителя в виде String
-     * @param srcRequisite номер реквизитов отправителя в виде String
-     * И конечного пользователя
-     * @param destPassport номер паспорта получателя в виде String
-     * @param destRequisite номер реквизитов получателя в виде String
-     * А также сумму
-     * @param amount
-     * В ходе работы метода происходит поиск счетов по реквизитам и паспорту
-     * И проверка баланса
-     * @return изменения состояния обоих счетов и возвращение true
-     */
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
