8ce54c215cedc6f4bae3709b2d18997cbf911456
MrrocketR <kupsovns@gmail.com>
2021-12-01 19:50:57 +0300

    5.6. Optional. Методы orElseThrow()

--- src/main/java/ru/job4j/exesJob/lamd/lamdEx/OptionalOrElseThrow.java
+++ src/main/java/ru/job4j/exesJob/lamd/lamdEx/OptionalOrElseThrow.java
@@ -21,13 +21,28 @@ public class OptionalOrElseThrow {
     public static class UserNotFoundException extends RuntimeException { }
 
     public static User orElseThrow(List<User> list, String login) {
-        // return search...;
+        Optional<User> user = search(list,login);
+        return  user.orElseThrow(UserNotFoundException::new);
     }
 
     private static Optional<User> search(List<User> list, String login) {
         Optional<User> user = Optional.empty();
-        //
+        for (User u: list ) {
+            if(u.getLogin().equals(login))
+            {
+                user = Optional.of(u);
+                break;
+            }
+
+        }
         return user;
     }
 
+    public static void main(String[] args) {
+        User u1 = new User("u1");
+        User u2 = new User("u2");
+        User u3 = new User("u3");
+        System.out.println( OptionalOrElseThrow.orElseThrow(List.of(u1, u2, u3), "u2"));
+
+    }
 }
