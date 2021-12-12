e5d3b2937066b8a82c7c838f3fba41336b427a0e
MrRocketR <kuptsovns@gmail.com>
2021-12-12 15:18:56 +0300

    Промежуточные операции

--- src/main/java/ru/job4j/exercises/streams/DistinctForPrimitive.java
+++ src/main/java/ru/job4j/exercises/streams/DistinctForPrimitive.java
@@ -8,9 +8,6 @@ import java.util.stream.IntStream;
 
 public class DistinctForPrimitive {
     public static List<Integer> collect(int[] data) {
-        int num = Arrays.stream(data)
-                .min()
-                .getAsInt(); // returns 7
 
         return new ArrayList<Integer>();
     }
