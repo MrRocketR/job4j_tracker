e01f4ccf326bd04c180e973c1d13ba20b260f2e6
MrRocketR <kuptovns@gmail.com>
2021-12-15 17:43:39 +0300

    Java Stream API

--- src/main/java/ru/job4j/exercises/TestStreams.java
+++ src/main/java/ru/job4j/exercises/TestStreams.java
@@ -1,6 +1,7 @@
 package ru.job4j.exercises;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -28,5 +29,15 @@ public class TestStreams {
                 .forEach(System.out::println);
 
         List.of(1, 2, 3).stream().forEach(n -> System.out.println(n));
+
+        int a1 = 2;
+        int a2 = 1;
+        int a3 = 4;
+        Stream.builder().add(a1).add(a2).add(a3).build();
+        System.out.println("reduce");
+        Collection<Integer> collection = Arrays.asList(1, 2, 3, 4, 5);
+        collection.stream().reduce((s1, s2) -> s1 + s2).orElse(0);
+        collection.forEach(System.out::println);
+
     }
 }
--- src/main/java/ru/job4j/exercises/streams/DistinctForPrimitive.java
+++ src/main/java/ru/job4j/exercises/streams/DistinctForPrimitive.java
@@ -2,13 +2,14 @@ package ru.job4j.exercises.streams;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class DistinctForPrimitive {
     public static List<Integer> collect(int[] data) {
-
-        return new ArrayList<Integer>();
+        return  Arrays.stream(data).distinct().boxed().collect(Collectors.toList());
     }
 }
