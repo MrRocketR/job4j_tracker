416eddb70a75c3c04a4514a60ff3b44e7ba5d774
MrRocketR <kuptovns@gmail.com>
2021-12-22 00:14:59 +0300

    flatMap

--- src/main/java/ru/job4j/exercises/SimpleTest.java
+++ src/main/java/ru/job4j/exercises/SimpleTest.java
@@ -1,11 +1,16 @@
 package ru.job4j.exercises;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 public class SimpleTest {
     public static void main(String[] args) {
-        List<List<Integer>> list = new ArrayList<>();
+        Collection<String> collection = Arrays.asList("1,2,0", "4,5");
+        // получить все числовые значения, которые хранятся через запятую в collection
+        String[] number = collection.stream().flatMap((p) -> Arrays.asList(p.split(",")).stream()).toArray(String[]::new);
+        System.out.println("number = " + Arrays.toString(number)); // напечатает number = [1, 2, 0, 4, 5]
 
     }
 }
