aa81b9678a530acdf0825a4ea15ed93b2a7d6cb9
MrRocketR <kuptovns@gmail.com>
2021-12-23 21:53:59 +0300

    validate

--- src/main/java/ru/job4j/exercises/SimpleTest.java
+++ src/main/java/ru/job4j/exercises/SimpleTest.java
@@ -8,7 +8,6 @@ import java.util.List;
 public class SimpleTest {
     public static void main(String[] args) {
         Collection<String> collection = Arrays.asList("1,2,0", "4,5");
-        // получить все числовые значения, которые хранятся через запятую в collection
         String[] number = collection.stream().
                 flatMap((p) -> Arrays.asList(p.split(",")).
                         stream()).toArray(String[]::new);
