b2066da327863039ad7baed90078733b426c0552
MrRocketR <kuptsovns@gmail.com>
2022-10-26 22:34:50 +0300

    3. Liquibase. Интеграционные тесты

--- src/test/java/ru/job4j/tracker/SqlTrackerTest.java
+++ src/test/java/ru/job4j/tracker/SqlTrackerTest.java
@@ -1,6 +1,10 @@
 package ru.job4j.tracker;
 
-import org.junit.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import ru.job4j.tracker.SqlTracker;
 import ru.job4j.tracker.model.Item;
 
 import java.io.InputStream;
@@ -12,17 +16,17 @@ import java.util.List;
 import java.util.Properties;
 
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class SqlTrackerTest {
 
     private static Connection connection;
 
-    @Ignore
     @BeforeClass
     public static void initConnection() {
-        try (InputStream in = SqlTrackerTest.class.getClassLoader()
-                .getResourceAsStream("test.properties")) {
+        try (InputStream in = SqlTrackerTest.class.getClassLoader().
+                getResourceAsStream("test.properties")) {
             Properties config = new Properties();
             config.load(in);
             Class.forName(config.getProperty("driver-class-name"));
@@ -37,13 +41,11 @@ public class SqlTrackerTest {
         }
     }
 
-    @Ignore
     @AfterClass
     public static void closeConnection() throws SQLException {
         connection.close();
     }
 
-    @Ignore
     @After
     public void wipeTable() throws SQLException {
         try (PreparedStatement statement = connection.prepareStatement("delete from items")) {
@@ -51,82 +53,12 @@ public class SqlTrackerTest {
         }
     }
 
-    @Ignore
     @Test
     public void whenSaveItemAndFindByGeneratedIdThenMustBeTheSame() {
         SqlTracker tracker = new SqlTracker(connection);
-        Item item = tracker.add(new Item("item1"));
+        Item item = new Item("item");
+        tracker.add(item);
         assertThat(tracker.findById(item.getId()), is(item));
     }
 
-    @Ignore
-    @Test
-    public void whenSaveItemAndDelete() {
-        SqlTracker tracker = new SqlTracker(connection);
-        Item item = tracker.add(new Item("item1"));
-        Assert.assertTrue(tracker.delete(item.getId()));
-
-    }
-
-    @Ignore
-    @Test
-    public void whenSaveItemAndReplaced() {
-        SqlTracker tracker = new SqlTracker(connection);
-        Item item = tracker.add(new Item("item1"));
-        Item itemR = new Item("item2");
-        int id = item.getId();
-        Assert.assertTrue(tracker.replace(item.getId(), itemR));
-        Assert.assertEquals(tracker.findById(id).getName(), itemR.getName());
-    }
-
-    @Ignore
-    @Test
-    public void whenSaveItemAndReplacedAndSameName() {
-        SqlTracker tracker = new SqlTracker(connection);
-        Item item = tracker.add(new Item("item1"));
-        Item itemR = new Item("item2");
-        int id = item.getId();
-        tracker.replace(id, itemR);
-        Assert.assertEquals(tracker.findById(id).getName(), itemR.getName());
-    }
-
-    @Ignore
-    @Test
-    public void whenAddAndFindByName() {
-        SqlTracker tracker = new SqlTracker(connection);
-        Item item1 = tracker.add(new Item("item"));
-        Item item2 = tracker.add(new Item("item2"));
-        Item item3 = tracker.add(new Item("item3"));
-        assertThat(tracker.findByName("item"), is(List.of(item1)));
-    }
-
-    @Ignore
-    @Test
-    public void whenAddAndFindById() {
-        SqlTracker tracker = new SqlTracker(connection);
-        Item item1 = tracker.add(new Item("item"));
-        Item item2 = tracker.add(new Item("item2"));
-        Assert.assertEquals((tracker.findById(item1.getId())), item1);
-    }
-
-    @Ignore
-    @Test
-    public void whenAddAndFindAll() {
-        SqlTracker tracker = new SqlTracker(connection);
-        Item item1 = tracker.add(new Item("item1"));
-        Item item2 = tracker.add(new Item("item2"));
-        Item item3 = tracker.add(new Item("item3"));
-        assertThat(tracker.findAll(), is(List.of(item1, item2, item3)));
-    }
-
-    @Ignore
-    @Test
-    public void whenAddAndDeleteNull() {
-        SqlTracker tracker = new SqlTracker(connection);
-        Item item1 = tracker.add(new Item("item"));
-        tracker.delete(item1.getId());
-        Assert.assertNull(tracker.findById(item1.getId()));
-    }
-
 }
-
