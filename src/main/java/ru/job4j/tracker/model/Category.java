d910ccf930465f5afcd42cdc8712dd63aae83afb
MrRocketR <kuptsovns@gmail.com>
2022-11-18 15:20:52 +0300

    2. @Setter, @Getter и другие

--- src/main/java/ru/job4j/tracker/model/Category.java
+++ src/main/java/ru/job4j/tracker/model/Category.java
@@ -4,13 +4,13 @@ import lombok.*;
 
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @RequiredArgsConstructor
+@Getter
 public class Category {
 
-    @Getter
     @EqualsAndHashCode.Include
     @NonNull
     private int id;
-    @Getter
+
     @Setter
     private String name;
 }
